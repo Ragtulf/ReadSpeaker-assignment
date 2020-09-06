2f94ef1e87537a896f58bd06eab8a7c66a2159c6
Ragna <ragnaulfars@gmail.com>
2020-09-06 15:13:02 +0200

    js validation working

--- code/script.js
+++ code/script.js
@@ -1,27 +1,13 @@
 const validate = () => {
-  const email = document.getElementById("email")
-  const repeatEmail = document.getElementById("confirm-email")
-  let emailRegEx = \b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b;
-  // const emailResult = emailRegEx.test(email)
-
-  // if (!email) {
-  //   alert("Please fill in your email address")
-  // } else if (!repeatEmail) {
-  //   alert("Please repeat your email address")
-  // } else if (!email.value.match(emailRegEx)) {
-  //   alert("Please enter a valid email address")
-  // } else if (email !== repeatEmail) {
-  //   alert("The two email addresses need to be identical")
-  // } else {
-  //  return location.reload
-  // }
+  const email = document.getElementById("email").value
+  const repeatEmail = document.getElementById("confirm-email").value
+  const emailRegEx = /\S+@\S+\.\S+/
 
   if (!email) {
-    console.log("no email")
     alert("Please fill in your email address")
   } else if (!repeatEmail) {
     alert("Please repeat your email address")
-  } else if (!email.value.match(emailRegEx)) {
+  } else if (!email.match(emailRegEx)) {
     alert("Please enter a valid email address")
   } else if (email !== repeatEmail) {
     alert("The two email addresses need to be identical")
